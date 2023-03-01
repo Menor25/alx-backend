1e901d8d704f0ea3063129d7f069be2be29f529c
Menor25 <ajirimenor@gmail.com>
2023-03-01 21:44:18 +0100

    Create a queue with Kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,17 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: string,
+    message: string,
+}
+
+const job = queue.create('push_notification_code', jobData);
+
+job.save((error) => {
+    if (!error) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
